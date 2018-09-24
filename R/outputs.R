c83a3df230472088ab75466c37dd2893c4f00735
Louisa <louisahsmith@gmail.com>
2018-09-24 13:04:07 -0400

    Add notes template

--- R/outputs.R
+++ R/outputs.R
@@ -14,6 +14,22 @@ homework <- function(..., metadata = NULL) {
   # if(!type %in% c("pdf", "word")) stop("Other formats not yet supported")
 }
 
+notes <- function(..., metadata = NULL) {
+  #if(type == "pdf"){
+  pdf_document_format(...,
+                      format = "notes",
+                      template = "notes.tex",
+                      metadata = metadata)
+  # }
+  # if(type == "word"){
+  #   word_document_format(...,
+  #                       format = "homework",
+  #                       template = "homework.docx",
+  #                       metadata = "homework_word.yaml")
+  # }
+  # if(!type %in% c("pdf", "word")) stop("Other formats not yet supported")
+}
+
 slides <- function(..., metadata = NULL) {
   beamer_presentation_format(...,
                         format = "slides",
